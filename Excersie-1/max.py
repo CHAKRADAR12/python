4ba274afa4a2de95f48ba0ccc755ce4a7c940871
CHAKRADAR12 <224g1a3312@srit.ac.in>
2024-08-14 10:23:04 +0530

    Add files via upload
    first

---
+++ Excersie-1/max.py
@@ -0,0 +1,6 @@
+s="asanam"
+r=s[::-1]
+if s==r:
+    print("its is palindrome")
+else:
+    print("its is not palindrome")
---
+++ Excersie-1/unique.py
@@ -0,0 +1,4 @@
+l1=list(map(int,input().split()))
+for i in l1:
+    if l1.count(i)==1:
+print(i)
